bce3f19d738bd306b3ed3d2166eca058da87b5b1
Tamas Gal <himself@tamasgal.com>
2015-01-23 18:05:04 +0100

    Initial commit

---
+++ royfit.py
@@ -0,0 +1,46 @@
+import operator
+
+from km3pipe import Module
+
+class CoincidentHitSelector(Module):
+    def process(self, blob):
+        detector = blob['Detector']
+        hits = blob['EvtRawHits']
+        hits.sort(key=lambda x: x.time)
+
+        print("Number of raw hits: " + str(len(hits)))
+
+        om_hits = {}
+        for hit in hits:
+            omkey = detector.pmtid2omkey(hit.pmt_id)
+            om_hits.setdefault((omkey[0], omkey[1]), []).append(hit)
+
+        merged_om_hits = {}
+        for om, hits in om_hits.iteritems():
+            print "raw hits:", len(hits)
+            hits_to_merge = None
+            for hit in hits:
+                print hit
+                if not hits_to_merge:
+                    hits_to_merge = [hit]
+                    continue
+                else:
+                    if hit.time - hits_to_merge[-1].time <= 10:
+                        hits_to_merge.append(hit)
+                    else:
+                        if len(hits_to_merge) > 1:
+                            merged_hit = reduce(operator.add, hits_to_merge)
+                            merged_om_hits.setdefault(om, []).append(merged_hit)
+                        hits_to_merge = [hit]
+
+            if merged_om_hits.has_key(om):
+                print "merged hits:", len(merged_om_hits[om])
+                for hit in merged_om_hits[om]:
+                    print hit
+        
+        return blob
+    
+
+class ROyFitter(Module):
+    pass
+
---
+++ run_royfit.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import os
+
+try:
+    import km3pipe
+except ImportError:
+    print("km3pipe could not be imported. Falling back to dev version.")
+    import sys
+    sys.path.insert(1, '/Users/tamasgal/Dev/km3pipe')
+    import km3pipe
+
+from km3pipe.pumps import EvtPump
+from km3modules import StatusBar, Geometry
+from royfit import CoincidentHitSelector
+
+DATA_PATH='/Users/tamasgal/Data/KM3NeT'
+EVT_FILE='mu_single_line/km3net_jul13_90m_muatm50T99_single_line.km3.jte.evt'
+GEO_FILE='Detector/km3net_single_line.detx'
+
+def main():
+    pipe = km3pipe.Pipeline()
+    pipe.attach(StatusBar)
+    pipe.attach(EvtPump, filename=os.path.join(DATA_PATH, EVT_FILE))
+    pipe.attach(Geometry, filename=os.path.join(DATA_PATH, GEO_FILE))
+    pipe.attach(CoincidentHitSelector)
+    pipe.drain(300)
+
+
+if __name__ == '__main__':
+    main()
